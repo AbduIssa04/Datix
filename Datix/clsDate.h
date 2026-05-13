b64ebc523ab5fa8a48f2908dc41795d317573db5
Abdu <abdurahmann.benissa@ucalgary.ca>
2026-05-13 09:06:00 -0400

    added DateToString, IsValid, leap year logic, and days/hours/minutes/seconds calculations

--- Datix/clsDate.h
+++ Datix/clsDate.h
@@ -133,5 +133,86 @@ public:
 	}
 
 
+	bool IsValid()
+	{
+		return IsValidDate(*this);
+	}
+
+	static string DateToString(clsDate Date)
+	{
+		return  to_string(Date.Day) + "/" + to_string(Date.Month) + "/" + to_string(Date.Year);
+	}
+
+	string DateToString()
+	{
+		return  DateToString(*this);
+	}
+
+	static bool isLeapYear(short Year)
+	{
+
+		// if year is divisible by 4 AND not divisible by 100
+	  // OR if year is divisible by 400
+	  // then it is a leap year
+		return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+	}
+
+	bool isLeapYear()
+	{
+		return isLeapYear(_Year);
+	}
+
+	static short NumberOfDaysInAYear(short Year)
+	{
+		return  isLeapYear(Year) ? 365 : 364;
+	}
+
+	short NumberOfDaysInAYear()
+	{
+		return  NumberOfDaysInAYear(_Year);
+	}
+
+	static short NumberOfHoursInAYear(short Year)
+	{
+		return  NumberOfDaysInAYear(Year) * 24;
+	}
+
+	short NumberOfHoursInAYear()
+	{
+		return  NumberOfHoursInAYear(_Year);
+	}
+
+	static int NumberOfMinutesInAYear(short Year)
+	{
+		return  NumberOfHoursInAYear(Year) * 60;
+	}
+
+	int NumberOfMinutesInAYear()
+	{
+		return  NumberOfMinutesInAYear(_Year);
+	}
+
+	static int NumberOfSecondsInAYear(short Year)
+	{
+		return  NumberOfMinutesInAYear(Year) * 60;
+	}
+
+	int NumberOfSecondsInAYear()
+	{
+		return  NumberOfSecondsInAYear();
+	}
+
+	static short NumberOfDaysInAMonth(short Month, short Year)
+	{
+
+		if (Month < 1 || Month>12)
+			return  0;
+
+		int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+		return (Month == 2) ? (isLeapYear(Year) ? 29 : 28) : days[Month - 1];
+
+	}
+
+
 };
 
