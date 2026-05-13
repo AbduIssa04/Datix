fb970efe83744c88862199a1a47529cff739c3b6
Abdu <abdurahmann.benissa@ucalgary.ca>
2026-05-13 09:05:07 -0400

    added getters, setters, properties for day/month/year, Print, GetSystemDate, and IsValidDate

--- Datix/clsDate.h
+++ Datix/clsDate.h
@@ -52,5 +52,86 @@ public:
 		_Year = Date1.Year;
 	}
 
+	void SetDay(short Day) {
+		_Day = Day;
+	}
+
+	short GetDay() {
+		return _Day;
+	}
+	__declspec(property(get = GetDay, put = SetDay)) short Day;
+
+	void SetMonth(short Month) {
+		_Month = Month;
+	}
+
+	short GetMonth() {
+		return _Month;
+	}
+	__declspec(property(get = GetMonth, put = SetMonth)) short Month;
+
+
+	void SetYear(short Year) {
+		_Year = Year;
+	}
+
+	short GetYear() {
+		return _Year;
+	}
+	__declspec(property(get = GetYear, put = SetYear)) short Year;
+
+	void Print()
+	{
+		cout << DateToString() << endl;
+	}
+
+	static clsDate GetSystemDate()
+	{
+		//system date
+		time_t t = time(0);
+		tm* now = localtime(&t);
+
+		short Day, Month, Year;
+
+		Year = now->tm_year + 1900;
+		Month = now->tm_mon + 1;
+		Day = now->tm_mday;
+
+		return clsDate(Day, Month, Year);
+	}
+
+	static	bool IsValidDate(clsDate Date)
+	{
+
+		if (Date.Day < 1 || Date.Day>31)
+			return false;
+
+		if (Date.Month < 1 || Date.Month>12)
+			return false;
+
+		if (Date.Month == 2)
+		{
+			if (isLeapYear(Date.Year))
+			{
+				if (Date.Day > 29)
+					return false;
+			}
+			else
+			{
+				if (Date.Day > 28)
+					return false;
+			}
+		}
+
+		short DaysInMonth = NumberOfDaysInAMonth(Date.Month, Date.Year);
+
+		if (Date.Day > DaysInMonth)
+			return false;
+
+		return true;
+
+	}
+
+
 };
 
